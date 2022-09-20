a05f70ca76dbca2b7777ca37dcc8bf1e9eb1f3ed
unknown <klammer.lukas@hotmail.com>
2022-09-20 10:30:43 +0200

    test click login button begin implement google login

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,23 @@
+// import { defineConfig } from 'cypress';
+
+// // Populate process.env with values from .env file
+// require('dotenv').config();
+
+// export default defineConfig {
 export default {
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  },
+  }
+
+  // ,
+
+  // env: {
+  //   googleClientId: '1651324181-ra4bdq8rm1rnrmc3649u6rgvlphm4tfr.apps.googleusercontent.com',
+  //   googleClientSecret: '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',
+  //   googleRefreshToken: '1//04erw4SpzqDR8CgYIARAAGAQSNwF-L9IrdYF7ox0qTzq6yztjuPEUis7ViHos0wYVajwy6r-ML8lQLOiPi2J_H97I_22jZiT0CgU',
+  // }
 };
+
+
